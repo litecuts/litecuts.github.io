ca6baacdfc61fafa9719490bf3ec9b8c1b2446eb
litecuts <71206543+litecuts@users.noreply.github.com>
2021-02-03 13:59:38 -0600

    2D Grid Assignment

--- Grid Neighbours/sketch.js
+++ Grid Neighbours/sketch.js
@@ -2,17 +2,9 @@ let grid = createEmptyGrid(3, 3);
 let rows, cols, cellWidth, cellHeight;
 let bgMusic;
 let clickSound;
-let tic;
-
-function preload() {
-  bgMusic = loadSound("assets/in_the_middle_of_nowhere_remix.ogg");
-  clickSound = loadSound("assets/click.wav");
-  tic = loadImage("assets/X.png");
-}
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  bgMusic.loop();
   rows = grid.length;
   cols = grid[0].length;
   cellWidth = width/cols;
@@ -25,20 +17,19 @@ function draw() {
 }
 
 function mousePressed() {
-  clickSound.play();
-
+  
   let x = Math.floor(mouseX / cellWidth);
   let y = Math.floor(mouseY / cellHeight);
 
-  toggleCell(x, y);   
-  toggleCell(x, y-1); 
-  toggleCell(x, y+1); 
-  toggleCell(x+1, y); 
-  toggleCell(x-1, y); 
+  toggleCell(x, y);   //self
+  toggleCell(x, y-1); //north
+  toggleCell(x, y+1); //south
+  toggleCell(x+1, y); //east
+  toggleCell(x-1, y); //west
 }
 
 function toggleCell(x, y) {
- 
+  //check that the coordinates are in the array
   if (x >= 0 && x < cols && y >= 0 && y < rows) {
     if (grid[y][x] === 1) {
       grid[y][x] = 0;
@@ -54,12 +45,12 @@ function displayGrid() {
   for (let y=0; y<rows; y++) {
     for (let x=0; x<cols; x++) {
       if (grid[y][x] === 0) {
-        fill("red");
+        fill("blue");
       }
       if (grid[y][x] === 1) {
-        fill("black");
+        fill("red");
       }
-      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight, tic);
+      rect(x*cellWidth, y*cellHeight, cellWidth, cellHeight);
     }
   }
 }
